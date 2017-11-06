cebf0f26441879517d98928acb90dad2b458ca89
Jens-Ole Skydt Andersen <josa@campusvejle.dk>
2017-11-06 08:05:23 +0100

    Uploaded files

---
+++ EksamenM2E2017.Opskrifter/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EksamenM2E2017.Opskrifter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<TestIngredientClass> ingredients = new List<TestIngredientClass>();
+
+            ingredients.Add(new TestIngredientClass(IngredientType.Grøntsag, "Hvidkål", 15));
+            ingredients.Add(new TestIngredientClass(IngredientType.Fisk, "Torsk", 30));
+            ingredients.Add(new TestIngredientClass(IngredientType.Oksekød, "Oksefars", 35));
+            ingredients.Add(new TestIngredientClass(IngredientType.Grøntsag, "Tomat", 15));
+            ingredients.Add(new TestIngredientClass(IngredientType.Mejeriprodukter, "Ost", 10));
+            ingredients.Add(new TestIngredientClass(IngredientType.Grøntsag, "Chili", 11));
+            ingredients.Add(new TestIngredientClass(IngredientType.Mel, "Hvedemel", 20));
+            ingredients.Add(new TestIngredientClass(IngredientType.Mejeriprodukter, "Gær", 20));
+            ingredients.Add(new TestIngredientClass(IngredientType.Mejeriprodukter, "Mælk", 12));
+            ingredients.Add(new TestIngredientClass(IngredientType.Kolonial, "Sukker", 30));
+            
+            DtgAllIngredients.ItemsSource = ingredients;
+
+            List<TestRecipeClass> recipes = new List<TestRecipeClass>();
+
+            List<TestIngredientClass> brød = new List<TestIngredientClass>();
+
+            brød.Add(ingredients[9]);
+            brød.Add(ingredients[8]);
+            brød.Add(ingredients[7]);
+            brød.Add(ingredients[6]);
+
+            recipes.Add(new TestRecipeClass(brød, "Brød"));
+
+            ListBoxRecipeList.ItemsSource = recipes;
+        }
+    }
+}
---
+++ EksamenM2E2017.Opskrifter/TestRecipeClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksamenM2E2017.Opskrifter
+{
+    public class TestRecipeClass
+    {
+        private List<TestIngredientClass> ingredientList;
+        private string recipeName;
+
+
+
+
+        public TestRecipeClass(List<TestIngredientClass> ingredients, string name)
+        {
+            IngredientList = ingredients;
+            RecipeName = name;
+        }
+
+
+        public string RecipeName
+        {
+            get { return recipeName; }
+            set { recipeName = value; }
+        }
+
+        public List<TestIngredientClass> IngredientList
+        {
+            get { return ingredientList; }
+            set { ingredientList = value; }
+        }
+
+
+        public override string ToString()
+        {
+            return RecipeName;
+        }
+    }
+}
